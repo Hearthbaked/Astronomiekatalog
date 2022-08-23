cb13fec8c08a3d08a3466516e5d768ac59759f9e
Hearthbaked <nikotin_one@hotmail.de>
2022-08-23 14:11:39 +0200

    Add files via upload

---
+++ Astronomiekatalog/Astronomiekatalog/Galaxie.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomiekatalog
+{
+    class Galaxie
+    {
+        private long idGalaxie;
+        private int anzahl_sterne;
+        private string galaxie_name;
+        private string galaxie_typ;
+        private string galaktischer_kern_schwarzes_loch;
+        private float galaxie_absolute_helligkeit;
+        private float galaxie_alter;
+        private float galaxie_geschwindigkeit;
+        private float masse_inkl_dunkle_materie;
+        private float galaxie_dicke;
+        private float galaxie_durchmesser;
+
+        public long GetIdGalaxie()
+        {
+            return idGalaxie;
+        }
+        public Galaxie(long idGalaxie, int anzahl_sterne, string galaxie_name, string galaxie_typ, string galaktischer_kern_schwarzes_loch, float galaxie_absolute_helligkeit, float galaxie_alter, 
+                       float galaxie_geschwindigkeit, float masse_inkl_dunkle_materie, float galaxie_dicke, float galaxie_durchmesser)
+        {
+            this.idGalaxie = idGalaxie;
+            this.anzahl_sterne = anzahl_sterne;
+            this.galaxie_name = galaxie_name;
+            this.galaxie_typ = galaxie_typ;
+            this.galaktischer_kern_schwarzes_loch = galaktischer_kern_schwarzes_loch;
+            this.galaxie_absolute_helligkeit = galaxie_absolute_helligkeit;
+            this.galaxie_alter = galaxie_alter;
+            this.galaxie_geschwindigkeit = galaxie_geschwindigkeit;
+            this.masse_inkl_dunkle_materie = masse_inkl_dunkle_materie;
+            this.galaxie_dicke = galaxie_dicke;
+            this.galaxie_durchmesser = galaxie_durchmesser;
+        }
+        public override string ToString()
+        {
+            return galaxie_name + "||||" + galaxie_typ + "||||" + galaktischer_kern_schwarzes_loch + "||||" + anzahl_sterne +"Milliarden"+ galaxie_absolute_helligkeit + "mag(Magnitude)" + "||||" + galaxie_alter + "Billionen Jahre" + "||||" + galaxie_geschwindigkeit +
+                   "km/s" + "||||" + masse_inkl_dunkle_materie + "Billionen Sonnenmassen" + "||||" + galaxie_dicke + "Lichtjahre" + "||||" + galaxie_durchmesser + "Lichtjahre";
+        }
+    }
+}
---
+++ Astronomiekatalog/Astronomiekatalog/Sternsystem.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Astronomiekatalog
+{
+    class Sternsystem
+    {
+        private long idSternsystem;
+        private int anzahl_sterne;
+        private string sternsystem_name;
+        private string sternsystem_typ;
+        private string asteroidenfelder;
+        private string sternsystem_stern;
+        private string spektralklasse;
+        private float sternsystem_alter;
+        private float sternsystem_masse;
+        private float sternsystem_absolute_helligkeit;
+
+        public long GetIdSternsystem()
+        {
+            return idSternsystem;
+        }
+
+        public Sternsystem(long idSternsystem, int anzahl_sterne, string sternsystem_name, string sternsystem_typ, string asteroidenfelder,
+                           string sternsystem_stern, string spektralklasse, float sternsystem_alter, float sternsystem_masse, 
+                           float sternsystem_absolute_helligkeit)
+        {
+            this.idSternsystem = idSternsystem;
+            this.anzahl_sterne = anzahl_sterne;
+            this.sternsystem_name = sternsystem_name;
+            this.sternsystem_typ = sternsystem_typ;
+            this.asteroidenfelder = asteroidenfelder;
+            this.sternsystem_stern = sternsystem_stern;
+            this.spektralklasse = spektralklasse;
+            this.sternsystem_alter = sternsystem_alter;
+            this.sternsystem_masse = sternsystem_masse;
+            this.sternsystem_absolute_helligkeit = sternsystem_absolute_helligkeit;
+        }
+
+        public override string ToString()
+        {
+            return anzahl_sterne + "Sterne" + "||||" + sternsystem_name + "||||" + sternsystem_typ + "||||" + asteroidenfelder + "||||" + sternsystem_stern + "||||" + spektralklasse +
+                  "||||" + sternsystem_alter + "Milliarden Jahre" + "||||" + sternsystem_masse + "Sonnenmassen" + "||||" + sternsystem_absolute_helligkeit + "mag(Magnitude)";
+        }
+    }
+}
